413aef4f982c04fa653a14d27c1bfd1bfb1788aa
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-11 00:17:53 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,12 @@
 /**
 *main - Determine if a random number is positive, negative or zero.
 *Return: 0 on success   
-
 */
 int main(void)
 {
-  
 int n; 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n=rand()-RAND_MAX/2;
 /* your code goes there */
 if (n < 0)
 {
