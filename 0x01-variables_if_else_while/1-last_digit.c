ec9bc465dcad32db1873f17b1ea366f8cc41fdb0
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-10 21:04:02 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                                                              *main - Determine if a random number is greater thand 5,less than 6 not 0,is 0
+/**         
+ *main - Determine if a random number is greater thand 5,less than 6 not 0,is 0
  *Return: 0 on success 
  */  
 int main(void)
 {
-  int n;
+    int n;
 
- srand(time(0));
- n = rand() - RAND_MAX / 2;
-/* your code goes there */
- if(n > 5)
- {
-  printf("%d is %s\n", n, "is greater than 5");
- }
- else if(n < 6 && n!=0)
- {
-  printf("%d is %s\n", n, "and is less than 6 and not 0");
- }  
- else                                                                    
- {  
-  printf("%d is %s\n", n, "and is 0");                                           }
+     srand(time(0));
+     n = rand() - RAND_MAX / 2;
+     /* your code goes there */
+    if(n > 5)
+    {
+      printf("%d is %s\n", n, "is greater than 5");
+    }
+    else if(n < 6 && n!=0
+    {
+      printf("%d is %s\n", n, "and is less than 6 and not 0");
+    }  
+    else                                                                    
+    {  
+      printf("%d is %s\n", n, "and is 0");                                           
+    }
  return (0);
 }
