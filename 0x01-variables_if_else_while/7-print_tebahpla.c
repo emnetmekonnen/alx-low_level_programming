5b7e1f3973992941f584fb2407ad89b4ab4d7c80
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-11 03:12:04 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * * main - Prints the lowercase alphabet in reverse.
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+* main - Prints the lowercase alphabet in reverse.
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
+char letter;
 
-	char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
 
+putchar('\n');
 
-
-	for (letter = 'z'; letter >= 'a'; letter--)
-
-		putchar(letter);
-
-
-
-	putchar('\n');
-
-
-
-	return (0);
-
+return (0);
 }
+
 
 
