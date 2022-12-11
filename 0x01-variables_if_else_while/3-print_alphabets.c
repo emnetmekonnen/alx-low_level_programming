8451ae9362fa381c265f4a33372ebe9855c4f17b
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-11 03:04:02 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,47 +3,42 @@
 
 
 /**
- *
- * * main - Prints all possible combinations of single-digit numbers.
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+* main - Prints all possible combinations of single-digit numbers.
+* Return: Always 0 (Success)
+* */
 
 int main(void)
 
 {
 
-	int num;
+int num;
 
 
 
-	for (num = 0; num <= 9; num++)
+for (num = 0; num <= 9; num++)
 
-	{
+{
 
-		putchar((num % 10) + '0');
+putchar((num % 10) + '0');
 
-		if (num == 9)
+if (num == 9)
 
-			continue;
-
-
-
-		putchar(',');
-
-		putchar(' ');
-
-	}
+continue;
 
 
 
-	putchar('\n');
+putchar(',');
+
+putchar(' ');
+
+}
 
 
 
-	return (0);
+putchar('\n');
+
+
+
+return (0);
 
 }
