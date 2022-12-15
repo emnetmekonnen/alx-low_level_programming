ea049a2ce002719b769ecf0520fa38da8228da26
emnetmekonnen <emnetmekonnen@gmail.com>
2022-12-15 01:58:32 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Function that prints the alphabet, in lowercase, followed by a new line.
+ * Return:0 on(success)
+ */
+
+void print_alphabet(void)
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+     
+_putchar('\n');
+}
