0e0f8a8b47a23c6f5875f7e4bab2d8b711fed42f
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-15 11:40:21 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
 *main-Program that prints _putchar,followed by a newline.
 *Return:0 on(success)
 */
-int main (void)
+int main(void)
 {
 char chr[8] = "_putchar";
 int i;
 for(i = 0; i < 8; i++)
 {
-_putchar (chr[i]);
+_putchar(chr[i]);
 }
-_putchar ('\n');
+_putchar('\n');
 return(0);
 }
