fa4c71bee6244ce1ee39a59fbe24a3f439e004fa
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-16 17:29:03 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,10 +2,10 @@
 
 
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer. 
- * starting from 00:00 to 23:59.
- * Return: nothing.
- */
+* jack_bauer - function that prints every minute of the day of Jack Bauer.
+* starting from 00:00 to 23:59.
+* Return: nothing.
+*/
 
 void jack_bauer(void)
 {
