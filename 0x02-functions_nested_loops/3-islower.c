b1c7b92bc0c7084924b698d2fc7e6172676bbac6
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-16 11:11:21 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 
 /**
- * _islower - function that checks for lowercase character. 
- * 
- * */
+* _islower - function that checks for lowercase character.
+*
+* */
 
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
 return (1);
-else                                
+else
 return (0);
 }
