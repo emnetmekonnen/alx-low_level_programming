0abf938525d743a82f5b22b2260606de5b779f85
emnetmekonnen <emnetmekonnen@users.noreply.github.com>
2022-12-16 09:54:35 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Print_alphabet_x10 - Prints the alphabet in lowercase.
- * Return:nothing.
- * */
+* Print_alphabet_x10 - Prints the alphabet in lowercase.
+* Return:nothing.
+* */
 
 void print_alphabet_x10(void)
 {
@@ -14,6 +14,6 @@ for (i = 0 ; i < 10; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
-_putchar('\n');         
-}                   
+_putchar('\n');
+}
 }
