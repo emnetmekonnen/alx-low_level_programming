b796aee9b2d3b13e15cc7e4aea64feddd5791e67
emnetmekonnen <emnetmekonnen@gmail.com>
2022-12-16 06:14:37 -0800

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+void jack_bauer(void)
+
+{
+
+	int x ;
+
+	int j ;
+
+	for(x=0; x <= 23 ; x++)
+
+	{
+
+		for(j=0; j<=59 ; j++)
+
+		{
+
+			_putchar((x / 10) + '0');
+
+			_putchar((x % 10) + '0');
+
+			_putchar(':');
+
+			_putchar((j / 10) + '0');
+
+			_putchar((j % 10) + '0');
+
+			_putchar('\n');
+
+		}		
+
+	}
+
+
+
+}
